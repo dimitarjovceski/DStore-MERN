cc5cfee6ad62aeb5f8ada92584744d1fb43b4419
Dimitar Jovcheski <d.jovceski5@gmail.com>
2024-04-26 14:21:29 +0200

    fix other small issues and complete the app

--- backend/index.js
+++ backend/index.js
@@ -34,11 +34,11 @@ app.use("/api/uploads", uploadRoutes);
 app.use("/api/orders", orderRoutes);
 
 const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 app.use("/uploads", express.static(path.join(__dirname + "/uploads")));
 
 app.get("*", (req, res ) => {
-  res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
+  res.sendFile(path.join(__dirname, "/frontend/dist/index.html"));
 });
 app.listen(port, () => console.log(`Server is running on PORT: ${port}`));
